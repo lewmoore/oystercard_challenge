3387fb94c8f4642939b36263c20d489db21b7f70
Simonbao <Simonbao.sb@gmail.com>
2018-01-10 12:01:19 +0000

    Step 13 Station Creation Test Refactored

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,11 +2,11 @@ require 'station'
 
 describe Station do
   subject(:station) { described_class.new('Barking', 4) }
-  it 'expects the station to have a zone' do
-    is_expected.to respond_to :zone
+  it 'expects the station to know it\'s name' do
+    expect(station.name).to eq('Barking')
   end
 
-  it 'expects station to have a name' do
-    is_expected.to respond_to :name
+  it 'expects station to have a zone' do
+    expect(station.zone).to eq(4)
   end
 end
